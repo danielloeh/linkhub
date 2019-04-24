a7df3fde138886538429a5ea03a453c5a24dd101
Daniel Loeffelholz <dloeffel@thoughtworks.com>
2019-04-24 11:04:24 +0200

    ignoring files

--- run_server_with_vars.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-AUTH_SERVER_URI=deal.eu.auth0.com \
-AUTH_CLIENT_ID=bBaaif7fBjlaIyc4ZjNKcab6ujFzajoa \
-EDIT_ENABLED=true \
-DATABASE_USER=postgres \
-DATABASE_PASSWORD=changeme \
-npm start
